872c517a1d6ed825e2226b72c1006dfe77628465
vleo1966 <Valleo2006@yandex.ru>
2024-09-11 02:33:05 +0300

    create lesson

---
+++ app/routes.py
@@ -0,0 +1,25 @@
+from flask import render_template, request
+from app import app
+
+
+@app.route("/", methods=["GET", "POST"])
+def user_form():
+    # Инициализация переменных
+    user_data = None
+
+    if request.method == "POST":
+        # Получаем данные из формы
+        name = request.form.get('name')
+        city = request.form.get('city')
+        hobby = request.form.get('hobby')
+        age = request.form.get('age')
+
+        # Сохраняем данные в виде словаря
+        user_data = {
+            'name': name,
+            'city': city,
+            'hobby': hobby,
+            'age': age
+        }
+
+    return render_template('form.html', user_data=user_data)
